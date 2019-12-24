1d135c813811b0c60c89daa39e1f900a4967be27
Jim Foltz <jim.foltz@gmail.com>
2019-12-24 09:46:06 -0500

    supply an optional initial filter

--- fzf/ps.cmd
+++ fzf/ps.cmd
@@ -14,6 +14,10 @@ set "F6=taskkill /f /im {1}"
 set "F9=toggle-preview"
 set "F10=taskmgr"
 
+if not "%1"=="" (
+	set "q=-q %1"
+)
+
 set "PREVIEW=wmic process where processid={2} get /format:list | findstr /v /e ="
 
 fzf ^
@@ -25,5 +29,6 @@ fzf ^
   --bind="F10:execute(%F10%)" ^
   --header="[ F1:size sort | F2:name sort | F5:kill (pid) | F6:kill (name) | F9:info | F10:taskmgr | ESC:exit ]" ^
   --preview="%PREVIEW%" ^
-  --preview-window="hidden"
+  --preview-window="hidden" ^
+  %q%
 
