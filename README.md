c938a9a7c364f5b064d7bcb22613fba3657777e9
Jim Foltz <jim.foltz@gmail.com>
2019-12-27 13:02:18 -0500

    update to ps.cmd and readme

--- README.md
+++ README.md
@@ -2,18 +2,27 @@
 
 Scripts and shortcuts mostly for working in Windows cmd.exe.
 
-ps.cmd - interactive fzf-based process viewer/killer. 
+### Process
+
+**ps.cmd**
+
+    interactive process viewer/killer. Requires `fzf`.
 
 
-![screenshot](https://imgur.com/WG9JkUV.png)
+
+### Navigation
+
+**cd__** [directory]
+
+    Change directory to %USERPROFILE%, and optionally a directory.
+
+**cd- [N]**
+
+    Go back 1 directory. With N, go back N levels.
+
+**fcd**
+
+    Fuzzy cd. Opens interactive directory list in the current directory. Type to filter. Use <kbd>up</kbd>/<kbd>down</kbd> to select.  Press  <kbd>ENTER</kbd> to go to the selected directory. Requires `fzf`
 
 
-
-Keys:
-        F1 - refresh the list and sort by size
-        F2 - refresh the list and sort by name
-        F5 - kill process by pid
-        F6 - kill process by name
-        F9 - toggle process info window (provided by wmic)
-        F10 - launch taskmgr
-        ESC - Quit
+[1] https://github.com/junegunn/fzf
--- ps.cmd
+++ ps.cmd
@@ -2,32 +2,32 @@
 @echo off
 setlocal
 
-set "FZF_DEFAULT_OPTS=--reverse --color=16,border:6"
-set "FZF_DEFAULT_COMMAND=tasklist /nh | sort /+65 /r"
+set "FZF_DEFAULT_OPTS=--reverse --color=16,border:6 --inline-info"
+set "FZF_DEFAULT_COMMAND=tasklist |findstr exe | sort /+65 /r | remove-blanks"
 
 set "F1=%FZF_DEFAULT_COMMAND%"
-set "F2=tasklist /nh | sort"
 
 set "F5=taskkill /f /pid {2}"
-set "F6=taskkill /f /im {1}"
+set "F5=taskkill /f /pid {2}"
+set "F7=taskkill /f /im {1}"
 
 set "F9=toggle-preview"
 set "F10=taskmgr"
 
 if not "%1"=="" (
-	set "q=-q %1"
+  set "q=-q %1"
 )
 
 set "PREVIEW=wmic process where processid={2} get /format:list | findstr /v /e ="
 
 fzf ^
   --bind="F1:reload(%F1%)" ^
-  --bind="F2:reload(%F2%)" ^
   --bind="F5:execute(%F5%)" ^
-  --bind="F6:execute(%F6%)" ^
+  --bind="enter:execute(%F5%)+accept" ^
+  --bind="F7:execute(%F7%)" ^
   --bind="F9:%F9%" ^
   --bind="F10:execute(%F10%)" ^
-  --header="[ F1:size sort | F2:name sort | F5:kill (pid) | F6:kill (name) | F9:info | F10:taskmgr | ESC:exit ]" ^
+  --header="[ F1:refresh | F5:kill (pid) | F7:kill (name) | F9:info | F10:taskmgr | ESC:exit ]" ^
   --preview="%PREVIEW%" ^
   --preview-window="hidden" ^
   %q%
