5e2edc1443a4d3702887406b557021e90b375bd5
Jim Foltz <jim.foltz@gmail.com>
2020-03-21 06:12:13 -0400

    update resize.cmd to set both COLUMNS and LINES env vars

--- resize.cmd
+++ resize.cmd
@@ -1,4 +1,10 @@
-for /F "usebackq" %%g in (`cols`) do (
-  set COLUMNS=%%g
-  setx COLUMNS %%g
+@echo off
+REM Set the COLUMNS and LINES environment variable which is used by 
+REM ls.exe to determine the column width for file listings.
+REM https://u-tools.com/msls
+for /F "tokens=1,2 delims=, usebackq" %%a in (`console-size`) do (
+  setx COLUMNS %%a >NUL
+  set COLUMNS=%%a
+  setx LINES %%b >NUL
+  set LINES=%%b
 )
---
+++ src/console-size.c
@@ -0,0 +1,15 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int columns, lines;
+
+    // Use STD_ERROR_HANDLE - STD_OUTPUT_HANDLE changed with redirection.
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_ERROR_HANDLE), &csbi);
+    columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    lines = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+
+    printf("%d,%d", columns, lines);
+    return 0;
+}
