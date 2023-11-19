b77a68c32ad36eea0e0b662f2d6863c132ebecb3
Jim Foltz <jim.foltz@gmail.com>
2023-11-19 12:50:01 -0500

    updated uz.cmd

--- uz.cmd
+++ uz.cmd
@@ -1,23 +1,26 @@
 @echo off
 
+where /q unzip
+if %errorlevel% neq 0 (
+    echo Did not find program unzip.exe. Exiting.
+    exit /b
+)
 
 if [%1] == [] (
   echo Usage:
   echo   uz filename.zip - unzip filename to temp folder.
-  echo   uz [clean] - delete all folders starting with "uz-"
-  goto eof
+  echo   uz clean - delete all folders ending with "-uz"
+  goto :eof
 )
 
 if [%1] == [clean]  (
-  forfiles /m uz-* /c "cmd /c rd /q/s @file" 
-  goto eof
+  forfiles /m *-UZ /c "cmd /c if @isdir==TRUE rd /q/s @file" 
+  goto :eof
 )
 
-set "UZTEMP=uz-tmp%RANDOM%"
+set "UZTEMP=%1-UZ"
 md %UZTEMP%
 cd %UZTEMP%
 unzip ..\%1
-cd %UZTEMP%
 set UZTEMP=
 
-:eof
